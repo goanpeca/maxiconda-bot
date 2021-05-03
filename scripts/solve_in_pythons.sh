06f2184081cc56d63d42564cfb65009d64480e8e
nerohmot <horen.tom@gmail.com>
2021-05-03 15:06:15 +0200

    trial

--- scripts/solve_in_pythons.sh
+++ scripts/solve_in_pythons.sh
@@ -5,17 +5,19 @@
 
 set -ex
 
+eval $(conda shell.bash hook)
+
 # Latest 3.6 CPython
 conda create -n py36 mamba python=3.6 --yes
 conda activate py36
 mamba install --file ./maxiconda-bot/requirements.txt --yes
 python ./maxiconda-bot/scripts/solve.py
 
-# Latest 3.6 PyPy
-conda create -n pypy36 mamba pypy3.6 --yes
-conda activate pypy36
-mamba install --file ./maxiconda-bot/requirements.txt --yes
-pypy3.6 ./maxiconda-bot/scripts/solve.py
+# # Latest 3.6 PyPy
+# conda create -n pypy36 mamba pypy3.6 --yes
+# conda activate pypy36
+# mamba install --file ./maxiconda-bot/requirements.txt --yes
+# pypy3.6 ./maxiconda-bot/scripts/solve.py
 
 # Latest 3.7 CPython
 conda create -n py36 mamba python=3.6 --yes
@@ -23,11 +25,11 @@ conda activate py36
 mamba install --file ./maxiconda-bot/requirements.txt --yes
 python ./maxiconda-bot/scripts/solve.py
 
-# Latest 3.7 PyPy
-conda create -n pypy37 mamba pypy3.7 --yes
-conda activate pypy37
-mamba install --file ./maxiconda-bot/requirements.txt --yes
-pypy3.7 ./maxiconda-bot/scripts/solve.py
+# # Latest 3.7 PyPy
+# conda create -n pypy37 mamba pypy3.7 --yes
+# conda activate pypy37
+# mamba install --file ./maxiconda-bot/requirements.txt --yes
+# pypy3.7 ./maxiconda-bot/scripts/solve.py
 
 # Latest 3.8 CPython
 conda create -n py38 mamba python=3.8 --yes
